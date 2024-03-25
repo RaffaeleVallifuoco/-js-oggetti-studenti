aa16f1431669fd695e421abf453c6f7f5bc74b14
RaffaeleVallifuoco <ra.vallifuoco@outlook.it>
2024-03-25 12:00:10 +0100

    creating app

---
+++ Js/app.js
@@ -0,0 +1,73 @@
+console.log('STUDENTI  -TEST-');
+console.log('');
+
+// ARRAY OGGETTI STUDENTI
+
+const studentsArray = [
+    {
+        studentName : 'Raff',
+        studentLast : 'Val',
+        studentAge : '28',
+
+    },
+    {
+        studentName: 'Giorgio',
+        studentLast: 'Barbuto',
+        studentAge: '25',
+
+    },
+    {
+        studentName: 'Sandra',
+        studentLast: 'SenzaZucchero',
+        studentAge: '32',
+
+    },
+    {
+        studentName: 'Ilaria',
+        studentLast: 'Ilarioni',
+        studentAge: '21',
+
+    }
+
+]
+
+console.log(studentsArray);
+console.log('Stusenti inseriti nel database : ');
+
+// STAMPA NOME COGNOME STUDENTI
+
+// for (let i = 0; i < studentsArray.length; i++) {
+//     const studentElement = studentsArray[i];
+//     console.log(studentElement.studentName , studentElement.studentLast);
+    
+// }
+
+studentsArray.forEach(function(element, index) {
+    console.log(element.studentName , element.studentLast);
+})
+console.log('');
+
+// NUOVO STUDENTE
+
+let choice = false;
+choice = confirm('Vuoi inserire un nuovo studente ?');
+if(choice) {
+    console.log('NUOVO STUDENTE : ');
+    let studentName = prompt('Inserisci il nome');
+    const studentLast = prompt('Inserisci il cognome');
+    const studentAge = prompt('Inserisci età');
+    console.log(studentName, studentLast, studentAge);
+    const newStudent = {studentName, studentLast, studentAge};
+    console.log(newStudent);
+    const choice2 = confirm('Sei sicuro di voler inserire lo studente ?')
+    if(choice2) {
+        studentsArray.push(newStudent);
+        console.log('');
+        console.log('Studenti ora presenti nel database :');
+        studentsArray.forEach(function (element, index,) {
+            console.log(element.studentName, element.studentLast, ', età : ', element.studentAge);
+        })
+    }
+    
+
+}
